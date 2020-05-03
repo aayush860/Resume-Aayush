6f96e6f9d793985c5e2190d25e235396f6d6043d
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-05-03 18:05:35 +0530

    make it better

--- resume/views.py
+++ resume/views.py
@@ -2,8 +2,13 @@ print ('============================')
 from django.shortcuts import render
 from django.http import HttpResponse
 from projects.models import Projects
+from skills.models import Skills
+from certificates.models import Certificates
 # Create your views here.
 def helloworld(request):
     projectx=Projects.objects.all()
-    
-    return render(request,'index.html',{'proj':projectx})
+    certx=Certificates.objects.all()
+    skillx=Skills.objects.all()
+    return render(request,'index.html',{'proj':projectx,
+                                        'skillxx':skillx, 
+                                        'certs':certx})
--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Skills(models.Model):
+    name = models.CharField(max_length=300)
+    class_name = models.CharField(max_length=300)
+    width = models.IntegerField()
