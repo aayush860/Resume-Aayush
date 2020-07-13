e4310e64bb74c4fd42f3193fd307c42ff2507ff6
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-07-14 03:54:29 +0530

    Update settings.py

--- resume/settings.py
+++ resume/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'resumeaayush.herokuapp.com']
 
